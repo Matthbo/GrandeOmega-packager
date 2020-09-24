f870b90b3119a989a7b5f6124f7107cac4db9f24
Matthbo <matthijs.booman@hotmail.com>
2020-09-24 19:13:53 +0200

    Added patcher

--- bin/go.js
+++ bin/go.js
@@ -1,14 +1,13 @@
-const chalk = require("chalk")
-
-const fetch = require("node-fetch"),
-    fs = require("fs"),
-    fsPromises = fs.promises,
+const chalk = require("chalk"),
+    fetch = require("node-fetch"),
+    fsPromises = require("fs").promises,
     path = require("path"),
     util = require("util")
 
 module.exports = class GO {
-    constructor(baseLocation){
+    constructor(baseLocation, patcherLocation){
         this.baseLocation = path.normalize(baseLocation)
+        this.patcherLocation = path.normalize(patcherLocation)
     }
 
     async checkVersion(url){
@@ -31,13 +30,24 @@ module.exports = class GO {
 
         const exec = util.promisify(require('child_process').exec)
         
-        await exec("npm i", { cwd: this.baseLocation }).then((stdout, stderr) => {
-            console.log('stdout:', stdout)
-            console.error('stderr:', stderr)
-        }).catch(error => console.error(chalk.redBright(`Failed to install dependencies:\n${error.stack}`)))
+        await exec("npm i -s", { cwd: this.baseLocation }).then(() => 
+            console.log(chalk.greenBright("  Done"))
+        ).catch(error => 
+            console.error(chalk.redBright(`Failed to install dependencies:\n${error.stack}`))
+        )
     }
 
     async patch(){
+        console.log(chalk.blueBright("Patching files"))
 
+        const mappings = JSON.parse(await fsPromises.readFile(this.patcherLocation + '/mapping.json')),
+            patchesLocation = path.normalize(this.patcherLocation + "/patches/")
+
+        for (const patchFileLocation in mappings){
+            const sourceFileLocation = mappings[patchFileLocation],
+                patchFile = await fsPromises.readFile(patchesLocation + patchFileLocation)
+
+            await fsPromises.writeFile(this.baseLocation + `/${sourceFileLocation}`, patchFile)
+        }
     }
 }
