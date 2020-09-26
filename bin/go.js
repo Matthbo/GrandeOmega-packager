737be052ed747e2955b33ace5d37955f9db8895f
Matthbo <matthijs.booman@hotmail.com>
2020-09-26 02:39:23 +0200

    Added builder

--- bin/go.js
+++ bin/go.js
@@ -1,13 +1,16 @@
 const chalk = require("chalk"),
     fetch = require("node-fetch"),
+    electronBuilder = require("electron-builder"),
+    platform = electronBuilder.Platform,
     fsPromises = require("fs").promises,
     path = require("path"),
     util = require("util")
 
 module.exports = class GO {
-    constructor(baseLocation, patcherLocation){
+    constructor(baseLocation, patcherLocation, buildLocation){
         this.baseLocation = path.normalize(baseLocation)
-        this.patcherLocation = path.normalize(patcherLocation)
+        this.patcherLocation = path.normalize(patcherLocation),
+        this.buildLocation = path.normalize(buildLocation)
     }
 
     async checkVersion(url){
@@ -50,4 +53,79 @@ module.exports = class GO {
             await fsPromises.writeFile(this.baseLocation + `/${sourceFileLocation}`, patchFile)
         }
     }
+
+    async build(){
+        console.log(chalk.blueBright("Building packages"))
+
+        const builder = new Builder(this.buildLocation, {
+            appId: "com.electron.grande-omega",
+            productName: "Grande Omega",
+            directories: {
+                app: this.baseLocation,
+                output: this.buildLocation + "/${os}"
+            }            
+        })
+
+        await builder.buildWin({
+            win: {
+                target: "msi"
+            }
+        })
+        // await builder.buildMac({
+        //     /* TODO */
+        // })
+        // await builder.buildLinux({
+        //     linux: {
+        //         target: "deb"
+        //     }
+        // })
+
+        console.log(chalk.greenBright("  Done"))
+    }
 }
+
+class Builder {
+    constructor(baseLocation, baseConfig){
+        this.winLocation = path.normalize(baseLocation + "/win")
+        this.macLocation = path.normalize(baseLocation + "/mac")
+        this.linuxLocation = path.normalize(baseLocation + "/linux")
+        this.baseConfig = baseConfig
+    }
+
+    async _getConfig(addCfg){
+            const cfg = {...this.baseConfig}
+    
+            for(const cfgEntry in addCfg){
+                cfg[cfgEntry] = addCfg[cfgEntry]
+            }
+
+            return cfg
+    }
+
+    async buildWin(addCfg){
+        console.log(chalk.blueBright("  Windows"))
+        const config = await this._getConfig(addCfg)
+
+        await electronBuilder.build({ 
+            targets: platform.WINDOWS.createTarget(),
+            config: config
+         }).catch(error => console.log(chalk.redBright(`  Failed:\n${error}`)))
+    }
+
+    async buildMac(config) {
+        console.log(chalk.blueBright("  MacOS"))
+        config = await this._getConfig(config)
+
+    }
+
+    async buildLinux(config) {
+        console.log(chalk.blueBright("  Linux"))
+        config = await this._getConfig(config)
+
+        await electronBuilder.build({
+            targets: platform.LINUX.createTarget(),
+            config: config
+        }).catch(error => console.log(chalk.redBright(`  Failed:\n${error}`)))
+    }
+}
+
